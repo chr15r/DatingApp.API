9d9475deae8ccaf77e1f71757b123e9ac41c9d16
Chris Roche <chrisroche02@hotmail.com>
2018-02-11 21:58:05 +0000

    Error handling

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -8,73 +8,76 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
 using Microsoft.Extensions.Configuration;
+using System;
 
 namespace DatingApp.API.Controllers
 {
     [Route("api/[controller]")]
     public class AuthController : Controller
     {
-        private readonly IAuthRepository _repo; 
-        private readonly IConfiguration _config; 
-        public AuthController(IAuthRepository repo, IConfiguration config) {
+        private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
+        {
             _repo = repo;
             _config = config;
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody]UserForRegisterDTO userForRegisterDTO){  
+        public async Task<IActionResult> Register([FromBody]UserForRegisterDTO userForRegisterDTO)
+        {
+            if (!string.IsNullOrEmpty(userForRegisterDTO.Username))
+                userForRegisterDTO.Username = userForRegisterDTO.Username.ToLower();
 
-            userForRegisterDTO.Username = userForRegisterDTO.Username.ToLower();
-            if(await _repo.UserExists(userForRegisterDTO.Username)) {
-                ModelState.AddModelError("Username","Username already exists");
+            if (await _repo.UserExists(userForRegisterDTO.Username))
+            {
+                ModelState.AddModelError("Username", "Username already exists");
             }
 
             // Validate Request
-            if (!ModelState.IsValid) 
-                return BadRequest(ModelState);       
-                
-            var userToCreate = new User {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var userToCreate = new User
+            {
                 Username = userForRegisterDTO.Username
             };
 
-            var createUser = await _repo.Register(userToCreate, userForRegisterDTO.Password);   
+            var createUser = await _repo.Register(userToCreate, userForRegisterDTO.Password);
 
-            return StatusCode(201); 
+            return StatusCode(201);
         }
 
         [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody] UserForLoginDTO userForLoginDTO) {
+        public async Task<IActionResult> Login([FromBody] UserForLoginDTO userForLoginDTO)
+        {
+            // throw new Exception("Go away!");
+
             var userFromRepo = await _repo.Login(userForLoginDTO.Username.ToLower(), userForLoginDTO.Password);
-            if (userFromRepo == null) 
+            if (userFromRepo == null)
                 return Unauthorized();
 
             // generate token
             var tokenhandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(_config.GetSection("AppSettings:Token").Value);
-            var tokenDescriptor = new SecurityTokenDescriptor {
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
                 Subject = new ClaimsIdentity(new Claim[]
                 {
-                    new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
-                    new Claim(ClaimTypes.Name, userFromRepo.Username)
+                        new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                        new Claim(ClaimTypes.Name, userFromRepo.Username)
                 }),
+
                 Expires = System.DateTime.Now.AddDays(1),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),SecurityAlgorithms.HmacSha512Signature)
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
             };
-            
-            try
-            {
-                var token = tokenhandler.CreateToken(tokenDescriptor);
-                var tokenString = tokenhandler.WriteToken(token);
-                return Ok(new {tokenString});
-            }
-            catch (System.Exception e)
-            {
-                var s = e.Message;
-                return Unauthorized();
-                throw;
-            }
-           
+            var token = tokenhandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenhandler.WriteToken(token);
+            return Ok(new { tokenString });
+
         }
-        
+
+
+
     }
 }
